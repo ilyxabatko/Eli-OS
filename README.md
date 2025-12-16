f8b82c84358527a574e77606f26a2191ffda8318
ilyxabatko <elijahbrnv@gmail.com>
2025-12-16 23:03:27 +0400

    minimal rust kernel & VGA writer

---
+++ README.md
@@ -0,0 +1,6 @@
+# **Eli OS** is an operating system project written in Rust.
+
+## Goal
+The goal of the project is to get familiar with OS development basics and implement a simple operating system that can be run on real hardware.
+
+## TODO the rest of README
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod macros;
+mod vga_buffer;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, {}", "Eli!");
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", &info);
+    loop {}
+}
